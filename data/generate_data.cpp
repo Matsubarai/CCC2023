70446e6375eba113ab7ca82bb2f2d9ac84617727
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-08 20:11:14 +0800

    Update generate_data.cpp

--- data/generate_data.cpp
+++ data/generate_data.cpp
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <math.h>
 
+using namespace std;
+
 int* tile(int* input_buffer, int buffer_width, int buffer_height, int tile_width, int tile_height, int tile_x, int tile_y) {
 
     int tile_size = tile_width * tile_height;
@@ -82,16 +84,23 @@ void kernel_cal(int* kernel_buffer, int tile_width, int tile_height, int* kernel
 }
 
 void write_to_file(int array[], int size, const char filename[]) {
-	std::ofstream outfile;
-	outfile.open(filename);
-	for (int i = 0; i < size; i++) {
-		outfile << array[i] << std::endl;
-	}
-	outfile.close();
+    fstream ioFile(filename, ios::out|ios::app);
+    if(!ioFile) {
+        cout << "cannot open " << filename << endl;
+        exit(0);
+    }
+    ioFile.seekg(0, ios::end);
+    for (int i = 0; i < size; i++) {
+        ioFile << array[i] << endl;
+    }
+    ioFile.close();
 }
 
 
 int main(int argc, char** argv) {
+
+    // 标准答案的存放文件
+    char ref_file[100] = "./ref.txt";
 
     // 输入图片的张数（此处默认图片只有一维）
     // 输入图片的宽度
@@ -120,6 +129,8 @@ int main(int argc, char** argv) {
 	int* img = new int[img_width * img_height];
 	int* img_out_ref = new int[img_width * img_height];
 
+    fstream ioFile;
+
     for (int n = 0; n < img_num; n++) {
 
         // 这里为了方便，直接通过代码来生成输入图片
@@ -130,18 +141,37 @@ int main(int argc, char** argv) {
         // 采用 readme 中的 padding 方式，对整张图片进行计算，得到参考结果
         kernel_cal(img, img_width, img_height, kernel, img_out_ref);
 
-        write_to_file(img_out_ref, img_width * img_height, "ref.txt");
+        // 删除之前实验已有的 ref_file
+        if (n == 0) {
+            ioFile.open(ref_file, ios::in);
+            if (ioFile) {
+                ioFile.close();
+                if (remove(ref_file) != 0) {
+                    cout << "Failed to delete file" << endl;
+                }
+            }
+        }
+        write_to_file(img_out_ref, img_width * img_height, ref_file);
 
         // 对当前的图片进行分块操作，并依次存到对应 kernel 的输入文件中
-        char file[100];
+        char input_file[100];
         for (int i = 0; i < blk_num_height; i++) {
             for (int j = 0; j < blk_num_width; j++) {
 
                 int* blk = tile(img, img_width, img_height, kernel_width, kernel_height, j, i);
 
-                sprintf(file, "./input%d.txt", (i * blk_num_width + j) % kernel_num + 1);
-
-                write_to_file(blk, kernel_width * kernel_height, file);
+                // 删除之前实验已有的 input_file
+                sprintf(input_file, "./input%d.txt", (i * blk_num_width + j) % kernel_num + 1);
+                if (n == 0 && ((i * blk_num_width + j) / kernel_num == 0)) {
+                    ioFile.open(input_file, ios::in);
+                    if (ioFile) {
+                        ioFile.close();
+                        if (remove(input_file) != 0) {
+                            cout << "Failed to delete file" << endl;
+                        }
+                    }
+                }
+                write_to_file(blk, kernel_width * kernel_height, input_file);
 
                 free(blk);
             }
@@ -149,6 +179,9 @@ int main(int argc, char** argv) {
 
         // 当前图片的大小不一定能整除 kernel 的个数
         // 若不能整除，需要增加若干全零的分块到最后一组分块中
+
+        // 这里有个特殊情况：所有分块的总和 < kernel 的个数
+        // 这种情况也在这部分一并处理
         int remain = (blk_num_height * blk_num_width) % kernel_num;
 
         if (remain != 0) {
@@ -157,15 +190,27 @@ int main(int argc, char** argv) {
 
                 int* blk = (int*)calloc(kernel_width * kernel_height, sizeof(int));
 
-                sprintf(file, "./input%d.txt", remain + i);
+                // 删除之前实验已有的 input_file
+                sprintf(input_file, "./input%d.txt", remain + i);
+                if (n == 0 && ((blk_num_height * blk_num_width) / kernel_num == 0)) {
+                    ioFile.open(input_file, ios::in);
+                    if (ioFile) {
+                        ioFile.close();
+                        if (remove(input_file) != 0) {
+                            cout << "Failed to delete file" << endl;
+                        }
+                    }
+                }
 
-                write_to_file(blk, kernel_width * kernel_height, file);
+                write_to_file(blk, kernel_width * kernel_height, input_file);
 
                 free(blk);
             }
 
         }
     }
+
+    printf("graph run time:%d", ceil(  (float)(blk_num_height * blk_num_width)   /   kernel_num  ) * img_num);
 	
 	delete [] img;
 	delete [] img_out_ref;
