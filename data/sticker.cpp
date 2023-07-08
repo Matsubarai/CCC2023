0507bde50a29afd8a6a9e0ed2bc5cb720933c3d7
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-08 20:10:46 +0800

    Update sticker.cpp

--- data/sticker.cpp
+++ data/sticker.cpp
@@ -7,6 +7,8 @@
 #include <cstring>
 #include <iomanip>
 
+using namespace std;
+
 void sticker(int* tile_buffer, int tile_width, int tile_height, int tile_x, int tile_y, int* output_buffer, int buffer_width, int buffer_height) {
 
     int x_tile_num = ceil((float)(buffer_width - tile_width) / (tile_width - 2)) + 1;
@@ -105,22 +107,29 @@ void sticker(int* tile_buffer, int tile_width, int tile_height, int tile_x, int 
     }
 }
 
-void read_to_mem(int data[], int size, const char filename[]) {
-	std::ifstream infile;
-	infile.open(filename);
+void read_to_mem(int data[], int size, const char filename[], int file_offset) {
+	fstream ioFile;
+	ioFile.open(filename, ios::in);
+    if (!ioFile) {
+        cout << filename << "cannot open" << endl;
+        exit(0);
+    }
 
+    ioFile.seekg(file_offset, ios::beg);
 	for (int i = 0; i < size; i++) {
 		std::string num;
-		getline(infile, num);
+		getline(ioFile, num);
 		while (num.c_str()[0] == 'T')
-			getline(infile, num);
+			getline(ioFile, num);
 		data[i] = stoi(num);
 	}
-	infile.close();
+	ioFile.close();
 }
 
 
 int main(int argc, char** argv) {
+    // 卷积结果参考文件
+    char ref_file[100] = "./ref.txt";
 
     // 输入图片的张数（此处默认图片只有一维）
     // 输入图片的宽度
@@ -147,71 +156,42 @@ int main(int argc, char** argv) {
     img_out_ref = (int *)malloc(sizeof(int) * img_width * img_height);
     blk_out = (int *)malloc(sizeof(int) * kernel_width * kernel_height);
 
-    char*  tmp;
-    FILE *fin;
     char file[100];
+    int file_offset;
 
     // 一张输入图片采用 readme 中的分块算法所得：
     // 宽度维度上的分块个数
     // 高度维度上的分块个数
     int blk_num_width = ceil((float)(img_width - kernel_width) / (kernel_width - 2)) + 1;
     int blk_num_height = ceil((float)(img_height - kernel_height) / (kernel_height - 2)) + 1;
-
+    
     for (int n = 0; n < img_num; n++) {
 
         // 拼接出当前图片的计算结果
+
+        // 当前图片的大小不一定能整除 kernel 的个数
+        // 若不能整除，若不能整除，在最后一组kernel计算的结果中，输入为零的那些分块对应的结果应该全部跳过
+        // 这里通过 file_offset 已经考虑了这种情况
         for (int i = 0; i < blk_num_height; i++) {
             for (int j = 0; j < blk_num_width; j++) {
                 
                 // 读出当前分块的数据
                 sprintf(file, "../Emulation-AIE/aiesimulator_output/data/output%d.txt", (i * blk_num_width + j) % kernel_num + 1);
-                if ((fin = fopen(file, "r")) == NULL) {
-                    printf("image file can not find\n");
-                    exit(0);
-                }
-                read_to_mem(blk_out, kernel_width * kernel_height, file);
+                file_offset = 2 * ((i * blk_num_width + j) / kernel_num) * kernel_width * kernel_height + 
+                                n * 2 * ceil((float)(blk_num_height * blk_num_width) / kernel_num) * kernel_width * kernel_height;
+                read_to_mem(blk_out, kernel_width * kernel_height, file, file_offset);
 
                 // 将当前分块结果拼接到最终输出中
                 sticker(blk_out, kernel_width, kernel_height, j, i, img_out, img_width, img_height);
 
-                fclose(fin);
-
             }
         }
-
-        // 当前图片的大小不一定能整除 kernel 的个数
-        // 若不能整除，将最后一组结果中，输入为零的那些分块对应的结果全部跳过
-        // int remain = (blk_num_height * blk_num_width) % kernel_num;
-
-        // if (remain != 0) {
-
-        //     for (int i = 1; i <= kernel_num - remain; i++) {
-
-        //         // 读出当前分块的数据
-        //         sprintf(file, "../Emulation-AIE/aiesimulator_output/data/output%d.txt", remain + i);
-        //         if ((fin = fopen(file, "r")) == NULL) {
-        //             printf("image file can not find\n");
-        //             exit(0);
-        //         }
-        //         read_to_mem(blk_out, kernel_width * kernel_height, file);
-
-        //         fclose(fin);
-        //     }
-
-        // }
 
         // 读出当前文件的参考结果
-        if ((fin = fopen("./ref.txt", "r")) == NULL) {
-            printf("image file can not find\n");
-            exit(0);
-        }
+        file_offset = n * img_width * img_height;
+        read_to_mem(img_out_ref, img_height * img_width, ref_file, file_offset);
 
-        for (int i = 0; i < img_height; i++) {
-            for (int j = 0; j < img_width; j++) {
-                fscanf(fin, "%d", &img_out_ref[i * img_width + j]);
-            }
-        }
-
+        // 使 AIE 结果与参考结果进行对比
         int erro = 0;
         for (int i = 0; i < img_height; i++) {
             for (int j = 0; j < img_width; j++) {
@@ -221,11 +201,8 @@ int main(int argc, char** argv) {
             }
         }
         printf("No.%d fig erro time:%d\n", (n + 1), erro);
-
-        fclose(fin);
     }
     
-    free(tmp);
     free(img_out);
     free(img_out_ref);
     free(blk_out);
