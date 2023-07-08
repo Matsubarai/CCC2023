6794f1368f92f57931e2dc654a864f24e76a015f
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-08 17:45:15 +0800

    Update sticker.cpp

--- data/sticker.cpp
+++ data/sticker.cpp
@@ -7,20 +7,6 @@
 #include <cstring>
 #include <iomanip>
 
-void read_to_mem(int data[], int size, const char filename[]) {
-	std::ifstream infile;
-	infile.open(filename);
-
-	for (int i = 0; i < size; i++) {
-		std::string num;
-		getline(infile, num);
-		while (num.c_str()[0] == 'T')
-			getline(infile, num);
-		data[i] = stoi(num);
-	}
-	infile.close();
-}
-
 void sticker(int* tile_buffer, int tile_width, int tile_height, int tile_x, int tile_y, int* output_buffer, int buffer_width, int buffer_height) {
 
     int x_tile_num = ceil((float)(buffer_width - tile_width) / (tile_width - 2)) + 1;
@@ -119,90 +105,107 @@ void sticker(int* tile_buffer, int tile_width, int tile_height, int tile_x, int 
     }
 }
 
+void read_to_mem(int data[], int size, const char filename[]) {
+	std::ifstream infile;
+	infile.open(filename);
+
+	for (int i = 0; i < size; i++) {
+		std::string num;
+		getline(infile, num);
+		while (num.c_str()[0] == 'T')
+			getline(infile, num);
+		data[i] = stoi(num);
+	}
+	infile.close();
+}
 
 
 int main(int argc, char** argv) {
+
+    // 输入图片的张数（此处默认图片只有一维）
+    // 输入图片的宽度
+    // 输入图片的高度
+    int img_num = 2;
+    int img_width = 3840;
+    int img_height = 2160;
+
+    // AIE kernel 的个数
+    // 一个 AIE kernel 能处理的图片宽度
+    // 一个 AIE kernel 能处理的图片高度
+    int kernel_num = 32;
+    int kernel_width = 64;
+    int kernel_height = 32;
+    
+    // 用来存储拼接好的利用 AIE 计算的结果
+    int* img_out;
+    // 参考结果
+    int* img_out_ref;
+    // 暂存一个 AIE kernel 的计算结果 
+    int* blk_out;
+
+    img_out = (int *)malloc(sizeof(int) * img_width * img_height);
+    img_out_ref = (int *)malloc(sizeof(int) * img_width * img_height);
+    blk_out = (int *)malloc(sizeof(int) * kernel_width * kernel_height);
+
+    char*  tmp;
     FILE *fin;
     char file[100];
 
-    int image_width = 1986;
-    int image_height = 32;
-    int tile_width = 64;
-    int tile_height = 32;
+    // 一张输入图片采用 readme 中的分块算法所得：
+    // 宽度维度上的分块个数
+    // 高度维度上的分块个数
+    int blk_num_width = ceil((float)(img_width - kernel_width) / (kernel_width - 2)) + 1;
+    int blk_num_height = ceil((float)(img_height - kernel_height) / (kernel_height - 2)) + 1;
 
-    int* image_out;
-    int* image_out_ref;
-    int* tile_out;
+    for (int n = 0; n < img_num; n++) {
 
-    char*  tmp;
+        // 拼接出当前图片的计算结果
+        for (int i = 0; i < blk_num_height; i++) {
+            for (int j = 0; j < blk_num_width; j++) {
+                
+                // 读出当前分块的数据
+                sprintf(file, "../Emulation-AIE/aiesimulator_output/data/output%d.txt", (i * blk_num_width + j) % kernel_num + 1);
+                if ((fin = fopen(file, "r")) == NULL) {
+                    printf("image file can not find\n");
+                    exit(0);
+                }
+                read_to_mem(blk_out, kernel_width * kernel_height, file);
 
-    image_out = (int *)malloc(sizeof(int) * image_width * image_height);
-    image_out_ref = (int *)malloc(sizeof(int) * image_width * image_height);
-    tile_out = (int *)malloc(sizeof(int) * tile_width * tile_height);
+                // 将当前分块结果拼接到最终输出中
+                sticker(blk_out, kernel_width, kernel_height, j, i, img_out, img_width, img_height);
 
-    int tile_x_num = ceil((float)(image_width - tile_width) / (tile_width - 2)) + 1;
-    int tile_y_num = ceil((float)(image_height - tile_height) / (tile_height - 2)) + 1;
+                fclose(fin);
 
-    for (int i = 0; i < tile_y_num; i++) {
-        for (int j = 0; j < tile_x_num; j++) {
-
-
-            sprintf(file, "../Emulation-AIE/aiesimulator_output/data/output%d.txt", (i * tile_x_num) + j+1);
-            if ((fin = fopen(file, "r")) == NULL) {
-                printf("image file can not find\n");
-                exit(0);
-            }
-
-            // read_to_mem(int data[], int size, const char filename[])
-            read_to_mem(tile_out, tile_width*tile_height, file);
-            // for (int y = 0; y < tile_height; y++) {
-            //     for (int x = 0; x < tile_width; x++) {
-            //         // fscanf(fin, "%[^\n]", tmp);
-            //         size_t len;
-            //         getline(&tmp, &len, fin);
-
-            //         if (y == (tile_height - 1) && (x == tile_width - 1)) {
-            //             // fscanf(fin, "%[^\n]", tmp);
-            //             getline(&tmp, &len, fin);
-            //         }
-
-            //         fscanf(fin, "%d", &tile_out[y * tile_width + x]);
-            //     }
-            // }
-
-            // void sticker(int* tile_buffer, int tile_width, int tile_height, int tile_x, int tile_y, int* output_buffer, int buffer_width, int buffer_height) 
-            sticker(tile_out, tile_width, tile_height, j, i, image_out, image_width, image_height);
-
-            fclose(fin);
-
-        }
-    }
-
-    if ((fin = fopen("./ref.txt", "r")) == NULL) {
-        printf("image file can not find\n");
-        exit(0);
-    }
-
-    for (int i = 0; i < image_height; i++) {
-        for (int j = 0; j < image_width; j++) {
-            fscanf(fin, "%d", &image_out_ref[i*image_width + j]);
-        }
-    }
-
-    int erro = 0;
-    for (int i = 0; i < image_height; i++) {
-        for (int j = 0; j < image_width; j++) {
-            if (abs(image_out[i * image_width + j] - image_out_ref[i * image_width + j]) > 1e-3) {
-                //printf("erro x:%d  erro y:%d\n", j, i);
-                erro++;
             }
         }
+
+        // 读出当前文件的参考结果
+        if ((fin = fopen("./ref.txt", "r")) == NULL) {
+            printf("image file can not find\n");
+            exit(0);
+        }
+
+        for (int i = 0; i < img_height; i++) {
+            for (int j = 0; j < img_width; j++) {
+                fscanf(fin, "%d", &img_out_ref[i * img_width + j]);
+            }
+        }
+
+        int erro = 0;
+        for (int i = 0; i < img_height; i++) {
+            for (int j = 0; j < img_width; j++) {
+                if (abs(img_out[i * img_width + j] - img_out_ref[i * img_width + j]) > 1e-3) {
+                    erro++;
+                }
+            }
+        }
+        printf("No.%d fig erro time:%d\n", (n + 1), erro);
+
+        fclose(fin);
     }
-    printf("erro time:%d\n", erro);
     
-    fclose(fin);
     free(tmp);
-    free(image_out);
-    free(image_out_ref);
-    free(tile_out);
+    free(img_out);
+    free(img_out_ref);
+    free(blk_out);
 }
