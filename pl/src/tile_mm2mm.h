28e5112820b36d689606d0a36e57f910d9625131
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-27 09:26:01 +0800

    Update tile_mm2mm.h
    add offset_img

--- pl/src/tile_mm2mm.h
+++ pl/src/tile_mm2mm.h
@@ -16,9 +16,12 @@ ap_int<data_width> *mem_out11, ap_int<data_width> *mem_out12, ap_int<data_width>
     unsigned tile_num_height = ceil((float)(img_height - tile_height) / (tile_height - 2)) + 1;
 
     unsigned count[15] = {0};
+    int mem_in_index;
 
     // 遍历所有图片
     for (unsigned i = 0; i < img_number; i++) {
+
+        unsigned offset_img = i * tile_num_width * tile_num_height * tile_width * tile_height;
 
         // 遍历所有的 tile
         for (unsigned j = 0; j < tile_num_height; j++) {
@@ -34,120 +37,118 @@ ap_int<data_width> *mem_out11, ap_int<data_width> *mem_out12, ap_int<data_width>
                 // 遍历当前的 tile
                 for (unsigned ti = 0; ti < tile_height; ti++) {
                     for (unsigned tj = 0; tj < tile_width; tj++) {
-
-                        int index = 0;
                         
                         // 判断是否遍历到图片边缘
                         bool edge_flag_width  = ((tj + offset_width) == (img_width));
                         bool edge_flag_height = ((ti + offset_height) == (img_height));
 
                         // 遍历到图片边缘后需要进行 padding 操作 
-                        // index == -1 表示补零
+                        // mem_in_index == -1 表示补零
                         if (!edge_flag_height && !edge_flag_width)
-                            index = (ti + offset_height) * img_width + tj + offset_width;                        
+                            mem_in_index = (ti + offset_height) * img_width + tj + offset_width + offset_img;                        
                         else if (edge_flag_height && !edge_flag_width)
-                            index = (img_height - 1) * img_width + tj + offset_width;
+                            mem_in_index = (img_height - 1) * img_width + tj + offset_width + offset_img;
                         else if (!edge_flag_height && edge_flag_width)
-                            index = (ti + offset_height) * img_width + img_width - 1;
+                            mem_in_index = (ti + offset_height) * img_width + img_width - 1 + offset_img;
                         else if (edge_flag_height && edge_flag_width)
-                            index = (img_height - 1) * img_width + img_width - 1;
+                            mem_in_index = (img_height - 1) * img_width + img_width - 1 + offset_img;
                         else
-                            index = -1;
+                            mem_in_index = -1;
 
                         // 将分块好的数据存入对应 aie 所读取的 mem 区域
                         switch(aie_index) {
                             case 1:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out1[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out1[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out1[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 2:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out2[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out2[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out2[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 3:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out3[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out3[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out3[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 4:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out4[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out4[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out4[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 5:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out5[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out5[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out5[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 6:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out6[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out6[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out6[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 7:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out7[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out7[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out7[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 8:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out8[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out8[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out8[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 9:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out9[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out9[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out9[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 10:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out10[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out10[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out10[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 11:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out11[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out11[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out11[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 12:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out12[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out12[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out12[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 13:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out13[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out13[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out13[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 14:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out14[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out14[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out14[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             case 15:
-                                if (index = -1)
+                                if (mem_in_index = -1)
                                     mem_out15[aie_index][count[aie_index]++] = 0;
                                 else 
-                                    mem_out15[aie_index][count[aie_index]++] = mem_in[index];
+                                    mem_out15[aie_index][count[aie_index]++] = mem_in[mem_in_index];
                                 break;
                             default:
-                                printf("aie index = %d\n", aie_index);
+                                printf("aie mem_in_index = %d\n", aie_index);
                         }
                     }
                 }
@@ -155,109 +156,3 @@ ap_int<data_width> *mem_out11, ap_int<data_width> *mem_out12, ap_int<data_width>
         }
     }
 }
-
-// template<unsigned data_width>
-// void mem_transfer(ap_int<data_width> *mem_in, 
-// ap_int<data_width> *mem_out1, ap_int<data_width> *mem_out2, ap_int<data_width> *mem_out3, ap_int<data_width> *mem_out4, 
-// ap_int<data_width> *mem_out5, ap_int<data_width> *mem_out6, ap_int<data_width> *mem_out7, ap_int<data_width> *mem_out8, ap_int<data_width> *mem_out9, ap_int<data_width> *mem_out10,
-// ap_int<data_width> *mem_out11, ap_int<data_width> *mem_out12, ap_int<data_width> *mem_out13, ap_int<data_width> *mem_out14, ap_int<data_width> *mem_out15) {
-
-//     switch(aie_index) {
-//         case 1:
-//             if (index = -1)
-//                 mem_out1[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out1[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 2:
-//             if (index = -1)
-//                 mem_out2[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out2[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 3:
-//             if (index = -1)
-//                 mem_out3[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out3[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 4:
-//             if (index = -1)
-//                 mem_out4[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out4[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 5:
-//             if (index = -1)
-//                 mem_out5[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out5[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 6:
-//             if (index = -1)
-//                 mem_out6[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out6[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 7:
-//             if (index = -1)
-//                 mem_out7[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out7[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 8:
-//             if (index = -1)
-//                 mem_out8[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out8[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 9:
-//             if (index = -1)
-//                 mem_out9[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out9[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 10:
-//             if (index = -1)
-//                 mem_out10[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out10[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 11:
-//             if (index = -1)
-//                 mem_out11[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out11[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 12:
-//             if (index = -1)
-//                 mem_out12[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out12[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 13:
-//             if (index = -1)
-//                 mem_out13[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out13[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 14:
-//             if (index = -1)
-//                 mem_out14[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out14[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         case 15:
-//             if (index = -1)
-//                 mem_out15[aie_index][count[aie_index]++] = 0;
-//             else 
-//                 mem_out15[aie_index][count[aie_index]++] = mem_in[index];
-//             break;
-//         default:
-//             printf("aie index = %d\n", aie_index);
-//     }
-
-// }
-//     std::map<int, char> mem_out_map;
-//     struct
-//     *mem_out[15]
