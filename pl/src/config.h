1fdb4048e1b4a30e92402e0af8df0ee3533f955a
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2023-07-27 11:18:35 +0800

    Update config.h

--- pl/src/config.h
+++ pl/src/config.h
@@ -8,3 +8,11 @@
 
 #define DWIDTH 32
 typedef qdma_axis<DWIDTH, 0, 0, 0> data;
+
+unsigned img_width = 720;
+unsigned img_height = 480;
+unsigned img_number = 2;
+unsigned tile_width = 64;
+unsigned tile_height = 32;
+unsigned data_width = 32;
+unsigned aie_kernel_number = 15;
