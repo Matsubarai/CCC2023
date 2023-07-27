45e60ad6941ea5a80f4348bb45136d367923392e
ForeverTime20 <kenforevertime@gmail.com>
2023-07-27 17:48:15 +0800

    update host

--- host/host.cpp
+++ host/host.cpp
@@ -88,15 +88,6 @@ int main(int argc, char** argv) {
     // img_in_buffer ---(PL)---> tiled_in_buffer_
     // std::array<xrt::bo, AIE_KERNEL_NUMBER> tiled_in_buffer_;
 
-    auto tiled_in_buffer_0 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(1));
-    auto tiled_in_buffer_1 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(2));
-    auto tiled_in_buffer_2 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(3));
-    auto tiled_in_buffer_3 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(4));
-    auto tiled_in_buffer_4 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(5));
-    auto tiled_in_buffer_5 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(6));
-    auto tiled_in_buffer_6 = xrt::bo(device, tile_size_in_bytes, tile_mm2mm_1.group_id(7));
-
-    
     // 用来存储 aie kernel 的输入数据
     // tiled_in_buffer ---(copy)---> in_buffer_
     // std::array<xrt::bo, AIE_KERNEL_NUMBER> in_buffer_;
@@ -117,19 +108,7 @@ int main(int argc, char** argv) {
     auto out_buffer_4 = xrt::bo(device, tile_size_in_bytes, s2mm_4.group_id(0));
     auto out_buffer_5 = xrt::bo(device, tile_size_in_bytes, s2mm_5.group_id(0));
     auto out_buffer_6 = xrt::bo(device, tile_size_in_bytes, s2mm_6.group_id(0));
-
     
-    // 用来存储 PL sticker 的输入数据
-    // out_buffer ---(copy)---> tiled_out_buffer_
-    auto tiled_out_buffer_0 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(0));
-    auto tiled_out_buffer_1 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(1));
-    auto tiled_out_buffer_2 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(2));
-    auto tiled_out_buffer_3 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(3));
-    auto tiled_out_buffer_4 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(4));
-    auto tiled_out_buffer_5 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(5));
-    auto tiled_out_buffer_6 = xrt::bo(device, tile_size_in_bytes, sticker_mm2mm_1.group_id(6));
-
-
     // 用来存储最后的计算结果
     // tiled_out_buffer_ ---(PL)---> img_out_buffer
     // 后续：device mem (img_out_buffer) ------> host mem
@@ -174,19 +153,9 @@ int main(int argc, char** argv) {
     std::cout << "Run the tile PL" << std::endl;
     auto run_tile_mm2mm_1 = tile_mm2mm_1(
 	    img_in_buffer, 
-	    tiled_in_buffer_0, tiled_in_buffer_1, tiled_in_buffer_2, tiled_in_buffer_3, tiled_in_buffer_4,
-	    tiled_in_buffer_5, tiled_in_buffer_6);
+	    in_buffer_0, in_buffer_1, in_buffer_2, in_buffer_3, in_buffer_4,
+	    in_buffer_5, in_buffer_6);
     run_tile_mm2mm_1.wait();
-
-    std::cout << "Copy tiled_in_buffer_ to in_buffer_" << std::endl;
-    in_buffer_0.copy(tiled_in_buffer_0, tile_size_in_bytes);
-    in_buffer_1.copy(tiled_in_buffer_1, tile_size_in_bytes);
-    in_buffer_2.copy(tiled_in_buffer_2, tile_size_in_bytes);
-    in_buffer_3.copy(tiled_in_buffer_3, tile_size_in_bytes);
-    in_buffer_4.copy(tiled_in_buffer_4, tile_size_in_bytes);
-    in_buffer_5.copy(tiled_in_buffer_5, tile_size_in_bytes);
-    in_buffer_6.copy(tiled_in_buffer_6, tile_size_in_bytes);
-
 
     std::cout << "Run the s2mm PL" << std::endl;
     auto run_s2mm_0 = s2mm_0(out_buffer_0, nullptr, tile_size_in_bytes);
@@ -238,20 +207,10 @@ int main(int argc, char** argv) {
     run_s2mm_6.wait();
     std::cout << "s2mm_6 completed" << std::endl;
 
-    std::cout << "Copy out_buffer_ to tiled_out_buffer" << std::endl;
-    tiled_out_buffer_0.copy(out_buffer_0, tile_size_in_bytes);
-    tiled_out_buffer_1.copy(out_buffer_1, tile_size_in_bytes);
-    tiled_out_buffer_2.copy(out_buffer_2, tile_size_in_bytes);
-    tiled_out_buffer_3.copy(out_buffer_3, tile_size_in_bytes);
-    tiled_out_buffer_4.copy(out_buffer_4, tile_size_in_bytes);
-    tiled_out_buffer_5.copy(out_buffer_5, tile_size_in_bytes);
-    tiled_out_buffer_6.copy(out_buffer_6, tile_size_in_bytes);
-
-
     std::cout << "Run the sticker PL" << std::endl;
     auto run_sticker_mm2mm_1 = sticker_mm2mm_1(
-	    tiled_out_buffer_0, tiled_out_buffer_1, tiled_out_buffer_2, tiled_out_buffer_3, tiled_out_buffer_4,
-	    tiled_out_buffer_5, tiled_out_buffer_6, 
+	    out_buffer_0, out_buffer_1, out_buffer_2, out_buffer_3, out_buffer_4,
+	    out_buffer_5, out_buffer_6, 
 	    img_out_buffer);
     run_sticker_mm2mm_1.wait();
 
