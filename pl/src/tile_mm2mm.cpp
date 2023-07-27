fe61163569b4b3e9c1aa1de4531e49722b3ad60a
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-27 18:36:26 +0800

    Update tile_mm2mm.cpp
    change style

--- pl/src/tile_mm2mm.cpp
+++ pl/src/tile_mm2mm.cpp
@@ -20,38 +20,38 @@ ap_int<DWIDTH> *mem_out5, ap_int<DWIDTH> *mem_out6, ap_int<DWIDTH> *mem_out7) {
     int mem_in_index;
 
     // 遍历所有图片
-    for (unsigned i = 0; i < img_number; i++) {
+    for (unsigned img_index = 0; img_index < img_number; img_index++) {
 
         // 已经处理过的图片对应的元素个数
-        unsigned offset_img = i * img_width * img_height;
+        unsigned offset_img = img_index * img_width * img_height;
 
         // 遍历所有的 tile
-        for (unsigned j = 0; j < tile_height_number; j++) {
-            for (unsigned k = 0; k < tile_width_number; k++) {
+        for (unsigned tile_index_height = 0; tile_index_height < tile_height_number; tile_index_height++) {
+            for (unsigned tile_index_width = 0; tile_index_width < tile_width_number; tile_index_width++) {
 
-                // 当前的 tile 应该传输给第 aie_index 个 aie kernel
-                unsigned aie_index = (j * tile_width_number + k) % AIE_KERNEL_NUMBER;
+                // 当前的 tile 应该传输给第 aie_index 个 aie kernel 计算
+                unsigned aie_index = (tile_index_height * tile_width_number + tile_index_width) % AIE_KERNEL_NUMBER;
                 
                 // 当前 tile 相对于第一个元素的偏移
-                unsigned offset_width  = k * (tile_width - 2);
-                unsigned offset_height = j * (tile_height - 2);
+                unsigned offset_width  = tile_index_width * (tile_width - 2);
+                unsigned offset_height = tile_index_height * (tile_height - 2);
 
                 // 遍历当前的 tile
-                for (unsigned ti = 0; ti < tile_height; ti++) {
-                    for (unsigned tj = 0; tj < tile_width; tj++) {
+                for (unsigned th = 0; th < tile_height; th++) {
+                    for (unsigned tw = 0; tw < tile_width; tw++) {
                         
                         // 判断是否遍历到图片边缘
-                        bool edge_flag_width  = ((tj + offset_width) == (img_width));
-                        bool edge_flag_height = ((ti + offset_height) == (img_height));
+                        bool edge_flag_width  = ((tw + offset_width)  == (img_width));
+                        bool edge_flag_height = ((th + offset_height) == (img_height));
 
                         // 遍历到图片边缘后需要进行 padding 操作 
                         // mem_in_index == -1 表示补零
                         if (!edge_flag_height && !edge_flag_width)
-                            mem_in_index = (ti + offset_height) * img_width + tj + offset_width + offset_img;                        
+                            mem_in_index = (th + offset_height) * img_width + tw + offset_width + offset_img;                        
                         else if (edge_flag_height && !edge_flag_width)
-                            mem_in_index = (img_height - 1) * img_width + tj + offset_width + offset_img;
+                            mem_in_index = (img_height - 1) * img_width + tw + offset_width + offset_img;
                         else if (!edge_flag_height && edge_flag_width)
-                            mem_in_index = (ti + offset_height) * img_width + img_width - 1 + offset_img;
+                            mem_in_index = (th + offset_height) * img_width + img_width - 1 + offset_img;
                         else if (edge_flag_height && edge_flag_width)
                             mem_in_index = (img_height - 1) * img_width + img_width - 1 + offset_img;
                         else
