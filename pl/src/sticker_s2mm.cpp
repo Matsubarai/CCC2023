a6d024d3c379085e9ec7732573dbc4c2003a875d
DongDongZZD <71597502+DongDongZZD@users.noreply.github.com>
2023-07-28 18:11:28 +0800

    Update sticker_s2mm.cpp------fix bugs

--- pl/src/sticker_s2mm.cpp
+++ pl/src/sticker_s2mm.cpp
@@ -68,55 +68,60 @@ ap_int<DWIDTH> *mem_out) {
 
                         if (tile_index_height == 0 && tile_index_width == 0) {
                             if (th >= 0 && th < tile_height - 1 && tw >= 0 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                mem_out[mem_out_index] = x;
                             }
                         }
-                        
+
                         else if (tile_index_height == 0 && tile_index_width > 0 && tile_index_width <= tile_num_width - 2) {
                             if (th >= 0 && th < tile_height - 1 && tw >= 1 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                mem_out[mem_out_index] = x;
                             }
                         }
 
                         else if (tile_index_width == 0 && tile_index_height > 0 && tile_index_height <= tile_num_height - 2) {
                             if (th >= 1 && th < tile_height - 1 && tw >= 0 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                mem_out[mem_out_index] = x;
                             }
                         }
 
                         else if (tile_index_width > 0 && tile_index_height > 0 && tile_index_width <= tile_num_width - 2 && tile_index_height <= tile_num_height - 2) {
                             if (th >= 1 && th < tile_height - 1 && tw >= 1 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                mem_out[mem_out_index] = x;
                             }
                         }
-                        
+
                         else if (tile_index_height == 0 && tile_index_width == tile_num_width - 1) {
                             if (th >= 0 && th < tile_height - 1 && tw >= 1 && tw < tile_width) {
-                                mem_out[mem_out_index] = x.data;
+                                if (tw + offset_width < img_width)
+                                    mem_out[mem_out_index] = x;
                             }
                         }
 
                         else if (tile_index_height > 0 && tile_index_height <= tile_num_height - 2 && tile_index_width == tile_num_width - 1) {
                             if (th >= 1 && th < tile_height - 1 && tw >= 1 && tw < tile_width) {
-                                mem_out[mem_out_index] = x.data;
+                                if (tw + offset_width < img_width)
+                                    mem_out[mem_out_index] = x;
                             }
                         }
 
                         else if (tile_index_width == 0 && tile_index_height == tile_num_height - 1) {
                             if (th >= 1 && th < tile_height && tw >= 0 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                if (th + offset_height < img_height)
+                                    mem_out[mem_out_index] = x;
                             }
                         }
-                        
+
                         else if (tile_index_height == tile_num_height - 1 && tile_index_width > 0 && tile_index_width <= tile_num_width - 2) {
                             if (th >= 1 && th < tile_height && tw >= 1 && tw < tile_width - 1) {
-                                mem_out[mem_out_index] = x.data;
+                                if (th + offset_height < img_height)
+                                    mem_out[mem_out_index] = x;
                             }
                         }
 
                         else if (tile_index_height == tile_num_height - 1 && tile_index_width == tile_num_width - 1) {
                             if (th >= 1 && th < tile_height && tw >= 1 && tw < tile_width) {
-                                mem_out[mem_out_index] = x.data;
+                                if ((th + offset_height < img_height) && (tw + offset_width < img_width))
+                                    mem_out[mem_out_index] = x;
                             }
                         }
                     }
