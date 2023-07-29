ccfb29132d02cd71621019c6965fb05fb06c3d52
DongDongZZD <zzd1411@mail.ustc.edu.cn>
2023-07-29 10:07:57 +0800

    v3.0 tile pl读数据时拼接

--- pl/src/tile_mm2s.cpp
+++ pl/src/tile_mm2s.cpp
@@ -15,8 +15,9 @@ hls::stream<data> &s4, hls::stream<data> &s5, hls::stream<data> &s6) {
 
     // 用作 mem_in 的索引
     int mem_in_index;
-    unsigned mem_in_index_group;
-    unsigned mem_in_index_idx;
+    unsigned mem_in_index_gid;
+    unsigned mem_in_index_uid;
+    ap_int<BUS_DWIDTH> mem_in_tmp;
     ap_int<DWIDTH> mem_tmp;
 
     // 遍历所有图片
@@ -36,59 +37,107 @@ hls::stream<data> &s4, hls::stream<data> &s5, hls::stream<data> &s6) {
                 unsigned offset_width  = tile_index_width * (tile_width - 2);
                 unsigned offset_height = tile_index_height * (tile_height - 2);
 
-                // 遍历当前的 tile
-                for (unsigned th = 0; th < tile_height; th++) {
-                    for (unsigned tw = 0; tw < tile_width; tw++) {
-                        
-                        // 遍历到图片边缘后需要进行 padding 操作 
-                        // mem_in_index == -1 表示补零
-                        if ((th + offset_height < img_height) && (tw + offset_width < img_width)) 
-                            mem_in_index = (th + offset_height) * img_width + tw + offset_width + offset_img;
-                        else if ((th + offset_height == img_height) && (tw + offset_width < img_width))
-                            mem_in_index = (img_height - 1) * img_width + tw + offset_width + offset_img;
-                        else if ((th + offset_height < img_height) && (tw + offset_width == img_width))
-                            mem_in_index = (th + offset_height) * img_width + img_width - 1 + offset_img;
-                        else if ((th + offset_height == img_height) && (tw + offset_width == img_width))
-                            mem_in_index = (img_height - 1) * img_width + img_width - 1 + offset_img;
-                        else
-                            mem_in_index = -1;
+                if (tile_index_height >= 0 && tile_index_height < tile_height_number - 1 
+                        && tile_index_width >= 0 && tile_index_width < tile_width_number - 1) {
+                    for (unsigned th = 0; th < tile_height; th++) {
+                        for (unsigned tw = 0; tw < tile_width; tw++) {
 
-                        data x;
-                        if (mem_in_index == -1)
-                            x.data = 0;
-                        else {
-                            mem_in_index_group = mem_in_index / DATA_NUM;
-                            mem_in_index_idx   = mem_in_index % DATA_NUM;
-                            mem_tmp = mem_in[mem_in_index_group].range((mem_in_index_idx + 1) * DWIDTH - 1, mem_in_index_idx * DWIDTH);
+                            mem_in_index     = (th + offset_height) * img_width + tw + offset_width + offset_img;
+                            mem_in_index_gid = mem_in_index / DATA_NUM;
+                            mem_in_index_uid = mem_in_index % DATA_NUM;
+
+                            if ((th * tile_width + tw) % DATA_NUM == 0) {
+                                mem_in_tmp = mem_in[mem_in_index_gid];
+                            }
+
+                            mem_tmp = mem_in_tmp.range((mem_in_index_uid + 1) * DWIDTH - 1, mem_in_index_uid * DWIDTH);
+
+                            data x;
                             x.data = mem_tmp;
+                            x.keep_all();
+                            switch(aie_index) {
+                                case 0:
+                                    s0.write(x);
+                                    break;
+                                case 1:
+                                    s1.write(x);
+                                    break;
+                                case 2:
+                                    s2.write(x);
+                                    break;
+                                case 3:
+                                    s3.write(x);
+                                    break;
+                                case 4:
+                                    s4.write(x);
+                                    break;
+                                case 5:
+                                    s5.write(x);
+                                    break;
+                                case 6:
+                                    s6.write(x);
+                                    break;
+                                default:
+                                    s0.write(x);
+                            }
                         }
+                    }
+                }
+
+                else {
+                    for (unsigned th = 0; th < tile_height; th++) {
+                        for (unsigned tw = 0; tw < tile_width; tw++) {
                             
-                        x.keep_all();
-                        // 将分块好的数据存入对应 aie 所读取的 mem 区域
-                        switch(aie_index) {
-                            case 0:
-                                s0.write(x);
-                                break;
-                            case 1:
-                                s1.write(x);
-                                break;
-                            case 2:
-                                s2.write(x);
-                                break;
-                            case 3:
-                                s3.write(x);
-                                break;
-                            case 4:
-                                s4.write(x);
-                                break;
-                            case 5:
-                                s5.write(x);
-                                break;
-                            case 6:
-                                s6.write(x);
-                                break;
-                            default:
-                                s0.write(x);
+                            // 遍历到图片边缘后需要进行 padding 操作 
+                            // mem_in_index == -1 表示补零
+                            if ((th + offset_height < img_height) && (tw + offset_width < img_width)) 
+                                mem_in_index = (th + offset_height) * img_width + tw + offset_width + offset_img;
+                            else if ((th + offset_height == img_height) && (tw + offset_width < img_width))
+                                mem_in_index = (img_height - 1) * img_width + tw + offset_width + offset_img;
+                            else if ((th + offset_height < img_height) && (tw + offset_width == img_width))
+                                mem_in_index = (th + offset_height) * img_width + img_width - 1 + offset_img;
+                            else if ((th + offset_height == img_height) && (tw + offset_width == img_width))
+                                mem_in_index = (img_height - 1) * img_width + img_width - 1 + offset_img;
+                            else
+                                mem_in_index = -1;
+
+                            data x;
+                            if (mem_in_index == -1)
+                                x.data = 0;
+                            else {
+                                mem_in_index_gid = mem_in_index / DATA_NUM;
+                                mem_in_index_uid = mem_in_index % DATA_NUM;
+                                mem_tmp = mem_in[mem_in_index_gid].range((mem_in_index_uid + 1) * DWIDTH - 1, mem_in_index_uid * DWIDTH);
+                                x.data = mem_tmp;
+                            }
+                                
+                            x.keep_all();
+                            // 将分块好的数据存入对应 aie 所读取的 mem 区域
+                            switch(aie_index) {
+                                case 0:
+                                    s0.write(x);
+                                    break;
+                                case 1:
+                                    s1.write(x);
+                                    break;
+                                case 2:
+                                    s2.write(x);
+                                    break;
+                                case 3:
+                                    s3.write(x);
+                                    break;
+                                case 4:
+                                    s4.write(x);
+                                    break;
+                                case 5:
+                                    s5.write(x);
+                                    break;
+                                case 6:
+                                    s6.write(x);
+                                    break;
+                                default:
+                                    s0.write(x);
+                            }
                         }
                     }
                 }
